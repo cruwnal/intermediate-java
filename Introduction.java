365f2c81693dea174622624faf44a606f7ba92d1
Kunal Nagwanshi <nagwanshikunal23@gmail.com>
2023-02-19 22:20:18 +0530

    Update Introduction.java

--- Introduction.java
+++ Introduction.java
@@ -1,5 +1,7 @@
 package com.kunal.access;
 
+import java.util.Scanner;
+
 public class Introduction {
     public static void main(String[] args) {
         int day = 1;
@@ -8,9 +10,12 @@ public class Introduction {
         String name = "kunal Nagwanshi";
         String hobbies = "reading,running";
         int age = 25;
+        Scanner in = new Scanner(System.in);
         System.out.println("My name is "+name);
         System.out.printf("I am %d years old.\n",age);
         System.out.print("My hobbies are "+hobbies);
+
+
 // System.out.printf("hello %s! today is Day: %d of your Intermediate java course",world,day);
     }
 }
