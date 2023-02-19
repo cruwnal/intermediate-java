f5d6b176504df9640855660f3c82cae66471ce72
Kunal Nagwanshi <nagwanshikunal23@gmail.com>
2023-02-19 19:11:31 +0530

    Add files via upload

---
+++ Introduction.java
@@ -0,0 +1,16 @@
+package com.kunal.access;
+
+public class Introduction {
+    public static void main(String[] args) {
+        int day = 1;
+        String world = "world";
+
+        String name = "kunal Nagwanshi";
+        String hobbies = "reading,running";
+        int age = 25;
+        System.out.println("My name is "+name);
+        System.out.printf("I am %d years old.\n",age);
+        System.out.print("My hobbies are "+hobbies);
+// System.out.printf("hello %s! today is Day: %d of your Intermediate java course",world,day);
+    }
+}
